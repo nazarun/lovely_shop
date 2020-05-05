d4f2d86bd39e3c810d13900b9aba5f92b863fc92
kterekh <62400448+kterekh@users.noreply.github.com>
2020-05-05 21:21:30 +0300

    Added js sorting function

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-$('.carousel').carousel({
-    interval: 4000
-    })
+// $('.carousel').carousel({
+//     interval: 4000
+//     })
+
+// class Options {
+//     constructor(elem) {
+//       this._elem = elem;
+//       elem.onclick = this.onClick.bind(this);
+//     }
+
+//     byName() {
+//     let myarray = getElementById(nav-home);
+     
+//     myarray.sort(function(a,b){ 
+//         return b - a;
+//     }
+
+//     fromLowtoHight() {
+      
+//     }
+
+//     fromHighttoLow() {
+      
+//     }
+
+//     onClick(event) {
+//       let action = event.target.dataset.action;
+//       if (action) {
+//         this[action]();
+//       }
+//     };
+//   }
+
+//   new Options(options);
+
+document.getElementById("byName").addEventListener("click", myFunction);
+
+function sortCards() {
+let array = [document.getElementById(first-card).value, document.getElementById(second-card).value, document.getElementById(third-card).value, document.getElementById(forth-card).value, document.getElementById(fifth-card).value, document.getElementById(six-card).value]
+   
+array.sort(function(a, b)
+{
+    return a - b;	
+});
+    
+alert(numbers);
+
+}
